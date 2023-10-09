cfdc997c2220af65c7cf27ea41d00ea7fa31c442
Dh-Kh <vsevolod.zaychenko@gmail.com>
2023-10-09 22:24:18 +0300

    bug fixing

--- logic/models.py
+++ logic/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 from .validators import validate_img_format
 
 class UserBehaviourModel(models.Model):
@@ -32,36 +30,7 @@ class PostModel(models.Model):
                     related_name="disliked_model", null=True, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     
-CREATE, READ, UPDATE, DELETE = "Create", "Read", "Update", "Delete"
-LOGIN, LOGOUT, LOGIN_FAILED = "Login", "Logout", "Login Failed"
-ACTION_TYPES = [
-    (CREATE, CREATE),
-    (READ, READ),
-    (UPDATE, UPDATE),
-    (DELETE, DELETE),
-    (LOGIN, LOGIN),
-    (LOGOUT, LOGOUT),
-    (LOGIN_FAILED, LOGIN_FAILED),
-]
 
-SUCCESS, FAILED = "Success", "Failed"
-ACTION_STATUS = [(SUCCESS, SUCCESS), (FAILED, FAILED)]
-
-class ActionLogModel(models.Model):
-    actor = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    action_type = models.CharField(choices=ACTION_TYPES, max_length=15)
-    action_time = models.DateTimeField(auto_now_add=True)
-    remarks = models.TextField(blank=True, null=True)
-    status = models.CharField(choices=ACTION_STATUS, max_length=7, default=SUCCESS)
-    data = models.JSONField(default=dict)
-    content_type = models.ForeignKey(
-        ContentType, models.SET_NULL, blank=True, null=True
-        )
-    object_id = models.PositiveIntegerField(blank=True, null=True)
-    content_object = GenericForeignKey()
-    
-    def __str__(self):
-        return f"{self.action_type} by {self.actor} on {self.action_time}"
 
     
 
